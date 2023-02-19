c0ff030445fd8a28db03fe709669781b89df4a98
Avi Santoso <avi.santoso@rise-x.io>
2023-02-19 15:14:03 +0800

    get the function working

--- Protect-String.ps1
+++ Protect-String.ps1
@@ -1,9 +1,10 @@
+Import-Module Pester
+
 function Protect-String {
     <#
-    As a Junior DevSecOps Engineer, you have been tasked with creating a PowerShell function to
-    automate the process of encrypting sensitive data using a symmetric encryption algorithm. Your
-    function should take in a plaintext string and a secret key, and return the encrypted string in
-    Base64 encoding.
+    Create a PowerShell function to automate the process of encrypting sensitive data using a
+    symmetric encryption algorithm. Your function should take in a plaintext string and a secret
+    key, and return the encrypted string in Base64 encoding.
     
     Use the Advanced Encryption Standard (AES) symmetric encryption algorithm with a block size of
     128 bits and a key size of 256 bits.
@@ -16,21 +17,43 @@ function Protect-String {
         [string]$SecretKey
     )
 
+    $blockSize = 128
+    $keySize = 256
+
     $aes = [System.Security.Cryptography.Aes]::Create()
-
-    $aes.BlockSize = 128;
-    $aes.KeySize = 256;
-
-    $aes.GenerateKey();
+    $aes.BlockSize = $blockSize;
+    $aes.KeySize = $keySize;
+    $aes.Key = [System.Text.Encoding]::UTF8.GetBytes($SecretKey)
     $aes.GenerateIV();
 
-    $iv = $aes.Key;
+    $iv = $aes.IV;
 
     $plainBytes = [System.Text.Encoding]::UTF8.GetBytes($PlainText);
-    
     $encryptedBytes = $aes.EncryptCbc($plainBytes, $iv);
-    
-    $encryptedStr = [System.Text.Encoding]::UTF8.GetString($encryptedBytes);
+    $encryptedStr = [System.Convert]::ToBase64String($encryptedBytes)
 
     return $encryptedStr
 }
+
+Describe "Protect-String" {
+    It "should pass" {
+        $true | Should -Be $true
+    }
+
+    Context "When given a plaintext string and secret key" {
+        It "should return the expected encrypted string" {
+            # Arrange
+            $plainText = "Hello World!"
+            $secretKey = "mySecretKey123"
+            $expectedResult = "oW6X2/fSTJc="
+
+            # Act
+            $result = Protect-String -PlainText $plainText -SecretKey $secretKey
+
+            # Assert
+            $result | Should -Be $expectedResult
+        }
+    }
+}
+
+Invoke-Pester
